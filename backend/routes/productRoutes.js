1f2eddf23e033f1508fe410b289b4fa375fe4079
DHEEMANTHSH <ruthikdheemanth@gmail.com>
2026-09-01 13:12:50 +0530

    Update project

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -22,10 +22,13 @@ router.post('/', async (req, res) => {
     }
 
     const unitOptions = [
-      { label: '500 gm', multiplier: 0.5 },
-      { label: '1 kg', multiplier: 1.0 },
-      { label: '2 kg', multiplier: 2.0 },
-      { label: '5 kg', multiplier: 5.0 }
+  { label: '100 gm', multiplier: 0.1 },
+  { label: '250 gm', multiplier: 0.25 },
+  { label: '500 gm', multiplier: 0.5 },
+  { label: '1 kg', multiplier: 1.0 },
+  { label: '2 kg', multiplier: 2.0 },
+  { label: '5 kg', multiplier: 5.0 },
+  { label: '10 kg', multiplier: 10.0 }
     ];
 
     const newProduct = new Product({
